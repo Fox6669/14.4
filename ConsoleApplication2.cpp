49080c73e367fb70edbf913380b723261dcce0da
FOx <vip.youtobe2016@mail.ru>
2022-06-10 16:15:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <stdint.h>
+#include <string>
+
+
+
+int main()
+{
+    std::cout << "enter your name : ";
+    std::string name;
+    std::getline ( std::cin, name );
+
+
+    std::cout << "enter your age : ";
+    std::string age;
+    std::getline(std::cin, age);
+
+    std::cout << name << " " << age << "\n";
+
+    std::cout << name. length() << "\n";
+    std::cout << age. length() << "\n";
+
+
+    
+}
+
